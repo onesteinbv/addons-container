ccf603ee4160168ff24468113e3fd2feb16d41dc
tarteo <d.sluijk@onestein.nl>
2023-06-09 14:30:38 +0200

    Add base_customer_user

---
+++ base_customer_user/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Customer User",
+    "version": "15.0.1.0.0",
+    "category": "Technical Settings",
+    "author": "Onestein",
+    "website": "https://www.onestein.nl",
+    "depends": ["base", "mail", "mail_bot"],
+    "data": [
+        "data/res_users_data.xml"
+    ]
+}
