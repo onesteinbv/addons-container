2743303db21db38f832bd1f616002ee3e6cf60de
Andrea Stirpe <a.stirpe@onestein.nl>
2023-06-26 09:29:49 +0200

    [ADD] l10n_nl_rgs_mis_report: tests

---
+++ l10n_nl_rgs_mis_report/tests/test_l10n_nl_rgs_mis_report.py
@@ -0,0 +1,28 @@
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from odoo.tests import HttpCase
+
+
+class TestL10nNlRgsMisReports(HttpCase):
+
+    def test_01_rgs_balance_sheet(self):
+        """"""
+        report = "l10n_nl_rgs_mis_report.mis_report_l10n_nl_rgs_balance_sheet"
+        instance = self.env["mis.report.instance"].create(
+            {
+                "name": "Balance Sheet",
+                "report_id": self.env.ref(report).id,
+            }
+        )
+        preview = instance.preview()
+
+    def test_02_rgs_profit_loss(self):
+        """"""
+        report = "l10n_nl_rgs_mis_report.mis_report_l10n_nl_rgs_profit_loss"
+        instance = self.env["mis.report.instance"].create(
+            {
+                "name": "Profit Loss",
+                "report_id": self.env.ref(report).id,
+            }
+        )
+        preview = instance.preview()
