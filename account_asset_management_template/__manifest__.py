c7fba36bc6c105698ebea5f2d266f967568592c6
Andrea Stirpe <a.stirpe@onestein.nl>
2023-06-26 09:43:23 +0200

    [ADD] account_asset_management_template

---
+++ account_asset_management_template/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Asset management template',
+    'version': '15.0.1.0.0',
+    "category": "Accounting & Finance",
+    'author': 'Onestein',
+    'website': 'http://www.onestein.eu',
+    'depends': [
+        'account_asset_management',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'license': 'LGPL-3',
+}
---
+++ account_asset_management_template/models/__init__.py
@@ -0,0 +1,3 @@
+from . import account_asset_group_template
+from . import account_asset_profile_template
+from . import account_chart_template
