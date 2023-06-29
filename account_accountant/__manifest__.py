2e44a802576b75ea2be9d3d8e7a6bc0ff1d2a706
tarteo <d.sluijk@onestein.nl>
2023-06-29 16:08:42 +0200

    Better readme

--- account_accountant/__manifest__.py
+++ account_accountant/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Fake Account Accountant",
-    "version": "15.0.0.0.1",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Onestein BV, André Schenkels",
     "category": "Accounting",
