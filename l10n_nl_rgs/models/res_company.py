646da247b726f576ab61806aa2b83a63e00806b8
Andrea Stirpe <a.stirpe@onestein.nl>
2023-07-04 14:29:23 +0200

    [IMP] l10n_nl_rgs: configuration rgs_type multicompany

---
+++ l10n_nl_rgs/models/res_company.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+
+class Company(models.Model):
+    _inherit = "res.company"
+
+    l10n_nl_rgs_type = fields.Selection([
+        ('rgs_basic', 'Basic'),
+        ('rgs_extended', 'Extended'),
+        ('rgs_ez', 'EZ / VOF'),
+        ('rgs_zzp', 'ZZP'),
+        ('rgs_bv', 'BV')
+    ])
--- l10n_nl_rgs/models/res_config_settings.py
+++ l10n_nl_rgs/models/res_config_settings.py
@@ -7,7 +7,18 @@ from odoo import api, fields, models, _
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
     
-    rgs_type = fields.Selection(
-        related='chart_template_id.rgs_type',
+    l10n_nl_rgs_type = fields.Selection(
+        related='company_id.l10n_nl_rgs_type',
         string='RGS Type',
         readonly=False)
+    is_l10n_nl_rgs = fields.Boolean(
+        compute="_compute_is_l10n_nl_rgs",
+    )
+
+    @api.depends("chart_template_id")
+    def _compute_is_l10n_nl_rgs(self):
+        for config in self:
+            if config.chart_template_id == self.env.ref('l10n_nl_rgs.l10nnl_rgs_chart_template'):
+                config.is_l10n_nl_rgs = True
+            else:
+                config.is_l10n_nl_rgs = False
