6c2958a384deac98df817a805bc0f5f870e02908
tarteo <d.sluijk@onestein.nl>
2023-07-12 13:59:27 +0200

    scripts/setup_smtp.py: Use base_mail_security

--- scripts/setup_smtp.py
+++ scripts/setup_smtp.py
@@ -9,12 +9,16 @@ import click_odoo
 @click.option("--password")
 def main(env, host, user, password):
     click.echo("Setup SMTP...")
+    module_mail_security = env.ref("base.module_base_mail_security")
+    if module_mail_security.state != "installed":
+        return click.echo(
+            "Module `base_mail_security` must be installed for this script to work",
+            err=True
+        )
 
-    count_server = env["ir.mail_server"].search([], count=True)
-    if count_server:
-        click.echo("Already have a SMTP server")
-        return
-
+    smtp_server = env["ir.mail_server"].search([
+        ("private", "=", True)
+    ])
     values = {
         "name": "Default SMTP",
         "smtp_authentication": "login",
@@ -24,8 +28,12 @@ def main(env, host, user, password):
         "smtp_host": host,
         "smtp_user": user,
         "smtp_pass": password,
+        "private": True
     }
-    env["ir.mail_server"].create(values)
+    if smtp_server:
+        smtp_server.write(values)
+    else:
+        env["ir.mail_server"].create(values)
 
 
 if __name__ == '__main__':
