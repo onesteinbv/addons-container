16a01294aa9782ba110e478d72f3623e6967f6b2
tarteo <d.sluijk@onestein.nl>
2023-11-16 14:24:53 +0100

    fix_stock_account

--- scripts/fix_stock_account.py
+++ scripts/fix_stock_account.py
@@ -10,12 +10,14 @@ def main(env):
     # See: accounts/chart_template.py in def _load(self, company) it deletes ir.property
     # This is a core issue it can also be triggered by creating a database installing stock_account installing belgium coa switch to it, and update stock_account
 
-    # TODO: Fix in core
+    # TODO: Fix in core, can be remove when done on existing databases
     xml_ids = [
         ("stock_account", "property_stock_account_output_categ_id"),
         ("stock_account", "property_stock_account_input_categ_id")
     ]
     main_company = env.ref('base.main_company', False)
+    if not main_company:  # Not sure if main company can be deleted
+        return
     for xml_id in xml_ids:
         ir_property = env.ref("%s.%s" % (xml_id[0], xml_id[1]), False)
         if ir_property:
