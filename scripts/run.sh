03c1d7e75ab0cf7d5714436f2d6ba0fff34da6a0
tarteo <d.sluijk@onestein.nl>
2023-11-22 11:33:16 +0100

    Remove odoo.com login in containers

--- scripts/run.sh
+++ scripts/run.sh
@@ -28,5 +28,5 @@ fi
 python /odoo/scripts/uninstall_auto_install_modules.py -c $ODOO_RC -d $DB_NAME --log-level=error
 
 if [[ -n "$KEYCLOAK_URL" ]]; then
-  python /odoo/scripts/setup_keycloak.py -c $ODOO_RC -d $DB_NAME --log-level=error --url "$KEYCLOAK_URL" --realm "$KEYCLOAK_REALM" --client-id "$KEYCLOAK_CLIENT_ID" --client-secret "$KEYCLOAK_CLIENT_SECRET"
+  python /odoo/scripts/setup_oauth.py -c $ODOO_RC -d $DB_NAME --log-level=error --url "$KEYCLOAK_URL" --realm "$KEYCLOAK_REALM" --client-id "$KEYCLOAK_CLIENT_ID" --client-secret "$KEYCLOAK_CLIENT_SECRET"
 fi
--- scripts/setup_oauth.py
+++ scripts/setup_oauth.py
@@ -40,6 +40,11 @@ def main(env, url, realm, client_id, client_secret):
     else:
         env["auth.oauth.provider"].create(values)
 
+    click.echo("Remove Odoo.com login...")
+    provider_openerp = env.ref("auth_oauth.provider_openerp", raise_if_not_found=False)
+    if provider_openerp:
+        provider_openerp.unlink()
+
 
 if __name__ == '__main__':
     main()
