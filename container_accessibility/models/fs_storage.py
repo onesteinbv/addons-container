91e6ab40e4908ff02a4b3d69c128ab011262740a
tarteo <d.sluijk@onestein.nl>
2023-12-12 14:13:06 +0100

    Allow fs_storage_backup mails

---
+++ container_accessibility/models/fs_storage.py
@@ -0,0 +1,11 @@
+from odoo import api, models
+
+
+class FSStorage(models.Model):
+    _inherit = "fs.storage"
+
+    def backup_db(self):
+        return super(FSStorage, self.with_context(allow_private_mail_server=True)).backup_db()
+
+    def cleanup_old_backups(self):
+        return super(FSStorage, self.with_context(allow_private_mail_server=True)).cleanup_old_backups()
