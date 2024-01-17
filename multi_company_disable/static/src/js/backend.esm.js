6ff4f46562f83b6e02b0a7b202a16e71ba8afafa
Mark Schuit <mark@gig.solutions>
2024-01-17 15:08:35 +0800

    [FIX] Pre-commit - JS - Renamed to esm.js and fixed imports

--- multi_company_disable/static/src/js/backend.esm.js
+++ multi_company_disable/static/src/js/backend.esm.js
@@ -1,8 +1,7 @@
 /** @odoo-module **/
-
-import { registry } from "@web/core/registry";
-import { } from "@web/webclient/switch_company_menu/switch_company_menu";
 import { MobileSwitchCompanyMenu } from "@web/webclient/burger_menu/mobile_switch_company_menu/mobile_switch_company_menu";
+import { registry } from "@web/core/registry";
+
 const { xml } = owl;
 
 registry.category("systray").remove("SwitchCompanyMenu")
