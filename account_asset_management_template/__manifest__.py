dc68f06396fbbbba39daa9d1af856c31ea7493c9
Mark Schuit <mark@gig.solutions>
2024-01-17 17:54:02 +0800

    [FIX] Pre-commit - Removed unused comment.

--- account_asset_management_template/__manifest__.py
+++ account_asset_management_template/__manifest__.py
@@ -1,7 +1,6 @@
 # Copyright 2023 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-# pylint: disable=file-not-used
 {
     "name": "Asset management template",
     "version": "16.0.1.0.0",
