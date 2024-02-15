e1a9999ddbc23761b59ff6a9d74e47de4aa56c01
tarteo <d.sluijk@onestein.nl>
2024-02-15 10:37:24 +0100

    Enable fix_stock_account temporary for existing databases

--- scripts/pre-update.sh
+++ scripts/pre-update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-# python /odoo/scripts/fix_stock_account.py -c $ODOO_RC -d $DB_NAME --log-level=error
+python /odoo/scripts/fix_stock_account.py -c $ODOO_RC -d $DB_NAME --log-level=error
