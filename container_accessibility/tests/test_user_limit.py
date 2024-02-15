24bb3b47fcce554cec2c7827f7f8ebf5dc58594e
tarteo <d.sluijk@onestein.nl>
2024-02-15 16:28:58 +0100

    Add tests regarding user limit

---
+++ container_accessibility/tests/test_user_limit.py
@@ -0,0 +1,20 @@
+from odoo.exceptions import UserError
+from odoo.tests.common import TransactionCase
+from odoo.tools import config
+
+
+class TestUserLimit(TransactionCase):
+    def test_exceeded(self):
+        config["user_limit"] = "5"
+        with self.assertRaises(UserError):
+            for i in range(10):
+                self.env["res.users"].create(
+                    {
+                        "name": "User %s" % i,
+                        "login": "User %s" % i,
+                        "groups_id": [
+                            self.ref("container_accessibility.group_restricted")
+                        ],
+                    }
+                )
+        config["user_limit"] = "0"
