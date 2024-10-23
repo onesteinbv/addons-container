17d5a2f484871f32552495f64029af92ddf5d8e3
tarteo <d.sluijk@onestein.nl>
2024-10-23 14:57:03 +0200

    [ADD] hr_install

---
+++ hr_install/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2017-2023 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Human Resources Installation",
+    "summary": "Install modules related to human resource management",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Human Resources",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "container_install",
+        "hr",
+        "hr_expense",
+        "hr_expense_remove_mobile_link",
+        "hr_employee_firstname",
+        "hr_timesheet_sheet",
+        "l10n_nl_bsn",
+        "l10n_nl_hr_recruitment",
+        "l10n_nl_hr_expense",
+    ],
+    "application": True,
+}
