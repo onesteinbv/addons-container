4982d310bba572700ab0271cccf56821e12394e9
tarteo <d.sluijk@onestein.nl>
2024-10-23 14:43:41 +0200

    [ADD] website_install

---
+++ website_install/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Website Installation",
+    "summary": "Install modules related to website building",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Website",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "container_install" "website",
+        "website_blog",
+        "website_odoo_debranding",
+        "website_onboarding",
+    ],
+    "application": True,
+}
