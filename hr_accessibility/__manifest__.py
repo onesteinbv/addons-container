e88e1e5a028c37748bdae86714e9daae03d3c72d
tarteo <d.sluijk@onestein.nl>
2024-10-23 14:11:30 +0200

    [REF] container_accessibility remove website dependency [REF] container_accessibility remove hr dependency [ADD] hr_accessibility

---
+++ hr_accessibility/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Container Accessibility",
+    "category": "Human Resources",
+    "version": "16.0.1.0.0",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "depends": [
+        "container_accessibility",
+        "hr",
+    ],
+    "data": [
+        "data/auditlog_rule_data.xml",
+    ],
+    "license": "LGPL-3",
+}
